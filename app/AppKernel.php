6ffaa712baaf837becba645d37e7f993374f17b6
Enrico <eantonel@gmail.com>
2014-04-21 15:23:25 +0200

    aggiunto foundation 5

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,8 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Bb\SiteBundle\BbSiteBundle(),
-            new Bmatzner\FoundationBundle\BmatznerFoundationBundle()
+            new Bmatzner\FoundationBundle\BmatznerFoundationBundle(),
+            new Bmatzner\ModernizrBundle\BmatznerModernizrBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
